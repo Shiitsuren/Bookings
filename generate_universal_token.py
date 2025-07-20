a7c6228de1455a990636220b33dca109364cad8e
Yash Pannu <yash pannu>
2025-07-20 18:58:28 +0530

    delete extra file

--- generate_universal_token.py
+++
@@ -1,7 +0,0 @@
-from django.contrib.auth.models import User
-
-# Create a universal API user if not exists
-user, created = User.objects.get_or_create(username='universal_api_user')
-if created:
-    user.set_password('universal_password')
-    user.save()
